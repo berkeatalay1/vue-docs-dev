0659ec310234f77c88a47a07ba002060cdceb8a5
berke.atalay1 <berke.atalay@beakwise.com>
2025-02-22 13:35:42 +0300

    feat: initilize setup.sh with error handling and repo URL

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,10 @@
-echo 'Hello from setup.sh'
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Variables
+REPO_URL="https://github.com/vuejs/v2.vuejs.org.git"
+
+# Placeholder for setup steps
+echo "Setting up development environment..."
