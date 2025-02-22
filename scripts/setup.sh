49c17f1b3ef0e9c25e2e592b77f9d50dedfb22cf
berke.atalay1 <berke.atalay@beakwise.com>
2025-02-22 13:46:31 +0300

    feat: Docker Installation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,5 +6,13 @@ set -e
 # Variables
 REPO_URL="https://github.com/vuejs/v2.vuejs.org.git"
 
-# Placeholder for setup steps
-echo "Setting up development environment..."
+# Update package list and install prerequisites
+echo "Updating package list and installing prerequisites..."
+apt-get update -y
+apt-get install -y curl git
+
+# Install Docker
+echo "Installing Docker..."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm get-docker.sh
