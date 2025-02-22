c41d17d6494126a71ab17ced44a1587ad2a3321e
berke.atalay1 <berke.atalay@beakwise.com>
2025-02-22 13:56:05 +0300

    feat: Install Docker-Compose

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,3 +16,7 @@ echo "Installing Docker..."
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 rm get-docker.sh
+
+echo "Installing Docker Compose..."
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
